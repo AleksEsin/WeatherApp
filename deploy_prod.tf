a261dd132b678ca7bf90d686ad9c5705fe45f150
Aleksey Esin <72644876+AleksEsin@users.noreply.github.com>
2021-09-27 15:24:30 +0300

    Update deploy_prod.tf

--- deploy_prod.tf
+++ deploy_prod.tf
@@ -10,23 +10,7 @@ resource "aws_instance" "prod" {
   ami                    = "ami-03d5c68bab01f3496" # Ubuntu_20.04
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.prod.id]
-  user_data              = <<EOF
-#!/bin/bash
-sudo apt update -y
-sudo apt install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update -y
-sudo apt install docker-ce docker-ce-cli containerd.io -y
-sudo docker run -p 8000:8000 -d yesinaleksey/weatherapp:v3
-EOF
+  user_data              = file("docker.sh")
 
   tags = {
     Name = "production"
