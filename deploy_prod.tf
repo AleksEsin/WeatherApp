0381928ddd8cf3602669bb666eb7465c9fa7c384
Aleksey Esin <72644876+AleksEsin@users.noreply.github.com>
2021-09-27 14:04:46 +0300

    Update deploy_prod.tf

--- deploy_prod.tf
+++ deploy_prod.tf
@@ -51,10 +51,6 @@ resource "aws_security_group" "prod" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 output "dev_public_ip" {
